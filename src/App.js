d810070cc7462194c9f8f2d8ccc586f983e1dc4a
zannujulius <zannujulius14@gmail.com>
2021-12-03 23:38:15 +0100

    fixed image 192x192 png

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 // import { ReactComponent as Logo } from "./logo.svg";
 import "./App.css";
 import Body from "./components/Body";
 import Nav from "./components/Nav";
 import nike from "./assets/images/nike.png";
 import usain from "./assets/images/usain.png";
-import SideNav from "./components/SideNav";
+// import SideNav from "./components/SideNav";
+// import "react-modern-calendar-datepicker/lib/DatePicker.css";
+// import DatePicker from "react-modern-calendar-datepicker";
+let deferredPrompt;
 
 function App() {
+  // const [selectedDay, setSelectedDay] = useState(null);
+  const [installable, setInstallable] = useState(false);
+
   const imageLink =
     "https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHNob2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=700&q=60";
-
   const handleLeft = () => {};
   const handleRight = () => {};
+
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+      alert(deferredPrompt, "line 15");
+      setInstallable(true);
+    });
+  }, []);
 
   return (
     <>
       <Nav />
       <Body>
         {/* <div className="slider">
-          <img src={imageLink} alt="sliderimage" className="img" lazy />
-        </div> */}
+            <img src={imageLink} alt="sliderimage" className="img" lazy />
+          </div> */}
         <div
           className="hero-cover"
           style={{
@@ -33,7 +48,6 @@ function App() {
             <div>Shop</div>
           </div>
         </div>
-
         {/* slider shoes */}
         <div className="new-btn__cover">
           <div className="btns">
@@ -65,7 +79,6 @@ function App() {
             </div>
           </div>
         </div>
-
         <div
           className="hero-cover"
           style={{
@@ -78,7 +91,6 @@ function App() {
             <div>Shop</div>
           </div>
         </div>
-
         <div className="gallery">
           {[1, 2, 3, 4, 5, 6].map((e, index) => (
             <div className="gallery-item" key={index}>
